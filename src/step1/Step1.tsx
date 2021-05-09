cc67639787761621488ed63e6d04d2843c20c356
doriasu <takuandtetsu@live.jp>
2021-05-09 15:29:45 +0900

    ファイル分割

--- src/step1/Step1.tsx
+++ src/step1/Step1.tsx
@@ -2,38 +2,24 @@
 /** @jsx jsx */
 import React, {useRef} from 'react'
 import {Canvas, MeshProps, useFrame} from 'react-three-fiber'
-import {Mesh} from 'three'
-import {css, jsx} from '@emotion/react'
+import {css, Global, jsx} from '@emotion/react'
+import emotionReset from 'emotion-reset'
+import {Work} from './src/work'
+const globalStyles = css`
+   ${emotionReset} 
+   *, *::after, *::before {
+       box-sizing: border-box;
+       -moz-osx-font-smoothing: grayscale;
+       -webkit-font-smoothing: antialiased;
+       font-smoothing: antialiased;
+   }
+`
 
-const theme = css`
-    width:100vw;
-    height:100vh;
-    background-color:#000;
-` 
-const Thing:React.FC=()=>{
-    const ref = useRef<Mesh>(null)
-    useFrame(()=>{if(ref.current){ref.current.rotation.z+=0.01}})
-    return(
-        <mesh
-         ref={ref}
-         onClick={e=>console.log('click')}
-         onPointerOver={e=>console.log('hover')}
-         onPointerOut={e=>console.log('unhover')}>
-             <planeBufferGeometry attach='geometry' args={[1,1]} />
-             <meshBasicMaterial
-             attach='material'
-             color='pink'
-             opacity={0.5}
-             transparent />
-         </mesh>
-    )
-}
 const Step1:React.FC=()=>{
     return(
-        <div css={theme}>
-        <Canvas>
-            <Thing />
-        </Canvas>
+        <div>
+            <Global styles={globalStyles} />
+        <Work />
         </div>
     )
 }
---
+++ src/step1/src/work.tsx
@@ -0,0 +1,45 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { css, jsx } from '@emotion/react';
+import { Mesh } from 'three'
+const theme = css`
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+`;
+
+const Thing = () => {
+    const ref = useRef<Mesh>(null)
+    useFrame(() => {
+        if (ref.current) {
+            ref.current.rotation.z += 0.01;
+        }
+    });
+
+    return (
+        <mesh
+            ref={ref}
+            onClick={e => console.log('click')}
+            onPointerOver={e => console.log('hover')}
+            onPointerOut={e => console.log('unhover')}
+        >
+            <planeBufferGeometry attach='geometry' args={[1, 1]} />
+            <meshBasicMaterial
+                attach='material'
+                color='hotpink'
+                opacity={0.5}
+                transparent
+            />
+        </mesh>
+    );
+};
+
+export const Work = () => (
+    <div css={theme}>
+        <Canvas>
+            <Thing />
+        </Canvas>
+    </div>
+);
