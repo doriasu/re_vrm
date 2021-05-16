6c842a97448109272a8d682dd3dcc4550a5514ef
doriasu <takuandtetsu@live.jp>
2021-05-16 22:30:02 +0900

    立方体で囲んだ

--- src/step1/src/Gravity.tsx
+++ src/step1/src/Gravity.tsx
@@ -15,11 +15,15 @@ type CubeProps={
     position:number[];
     mass:number;
 }
-const Plane:React.FC=()=>{
-    const [ref] = usePlane(()=>{return {rotation:[-Math.PI/2,0,0]}})
+type PlaneProps={
+    rotation:number[];
+    position:number[];
+}
+const Plane:React.FC<PlaneProps>=(props:PlaneProps)=>{
+    const [ref] = usePlane(()=>{return {rotation:props.rotation, position:props.position}})
     return(
         <mesh ref={ref}>
-            <planeBufferGeometry args={[1000,1000]} />
+            <planeBufferGeometry args={[100,100]} />
         </mesh>
     )
 }
@@ -37,7 +41,7 @@ const Spere:React.FC<CubeProps>=(props)=>{
 const Particles:React.FC=()=>{
     const particles = []
     for(let i =0;i<100;i++){
-        particles.push(<Spere position={[Math.random()*10-5, Math.random()*10-5, Math.random()*10-5]} mass={0.01} />)
+        particles.push(<Spere position={[Math.random()*10-5, Math.random()*10-5, Math.random()*10]} mass={0.01} />)
     }
     return(
         <Fragment>{particles}</Fragment>
@@ -58,9 +62,14 @@ const Gravity:React.FC=()=>{
         <div css={theme} >
         <Canvas camera={{position:[0, 5, 50]}}>
             <Controls />
-            <pointLight color='#FFFFFF' intensity={1} position={[0, 2000, 1000]} />
+            <pointLight color='#FFFFFF' intensity={1} position={[0, 25, 0]} />
             <Physics>
-                <Plane />
+                <Plane rotation={[-Math.PI/2,0,0]} position={[0,0,0]} />
+                <Plane rotation={[-Math.PI/2,-Math.PI/2,0]} position={[50,50,0]} />
+                <Plane rotation={[0,0,0]} position={[0,50,-50]} />
+                <Plane rotation={[-Math.PI/2,Math.PI/2,0]} position={[-50,50,0]} />
+                <Plane rotation={[Math.PI,0,0]} position={[0,50,50]} />
+                <Plane rotation={[-Math.PI*3/2,0,0]} position={[0,100,0]} />
                 {/* <Cube mass={1} position={[0,0,0]} /> */}
                 <Particles />
                 <Cube />
